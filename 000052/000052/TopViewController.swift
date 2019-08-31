c87b00aed6fc019526de285518ade7d85d5c8ed1
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-01 08:48:41 +0900

    000052

---
+++ 000052/000052/TopViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TopViewController.swift
+//  000052
+//
+//  Created by 酒井典昭 on 2019/09/01.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    @IBOutlet weak var topMainView: TopMainView!
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+        topMainView.delegate = self
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController: TopMainViewDelegate {
+    func tochedButton(_ sender: UIButton) {
+        let secondViewController = SecondViewController()
+        transitionViewController(from: self, to: secondViewController)
+    }
+    
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
---
+++ 000052/Pods/Target Support Files/Pods-000052/Pods-000052-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_000052 : NSObject
+@end
+@implementation PodsDummy_Pods_000052
+@end
